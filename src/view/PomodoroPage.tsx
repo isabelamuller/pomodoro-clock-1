4ad125638b533e846a76ed812f0827d66ca6f5e0
GustavoMilcharek <gmilcharek@degdigital.com>
2023-03-17 15:39:13 -0300

    Removes unneeded code.

--- src/view/PomodoroPage.tsx
+++ src/view/PomodoroPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { PageWrapper, ContentWrapper, ClockWrapper } from "./styles";
+import { PageWrapper, ContentWrapper } from "./styles";
 import Clock from "../components/Clock";
 import Header from "../components/Header";
 
--- src/view/styles.ts
+++ src/view/styles.ts
@@ -8,9 +8,3 @@ export const ContentWrapper = styled.div`
   width: 40rem;
   margin: auto;
 `;
-
-export const ClockWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 2rem;
-`;
